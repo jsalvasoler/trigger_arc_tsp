16a2d3c506763331a7264e93b8d870d97ed6b00a
jsalvasoler <jsalvasoler@hotmail.com>
2024-08-12 00:49:55 +0200

    refactor: refactor GurobiModel to make it less static

--- src/trigger_arc_tsp/gurobi_model.py
+++ src/trigger_arc_tsp/gurobi_model.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 
 import gurobipy as gp
@@ -27,58 +29,57 @@ def cleanup_instance_name(instance: str) -> str:
 class GurobiModel:
     def __init__(self, instance: Instance) -> None:
         self.instance = instance
+        self.model = None
 
-    def formulate(self) -> gp.Model:
-        model = gp.Model("TriggerArcTSP")
+        self.x, self.y, self.u = None, None, None
 
-        x = model.addVars(self.instance.edges, vtype=gp.GRB.BINARY, name="x")
-        y = model.addVars(self.instance.relations, vtype=gp.GRB.BINARY, name="y")
-        u = model.addVars(self.instance.nodes, vtype=gp.GRB.CONTINUOUS, name="u")
+    def formulate(self) -> None:
+        self.model = gp.Model("TriggerArcTSP")
 
-        model._x = x
-        model._y = y
-        model._u = u
+        self.x = self.model.addVars(self.instance.edges, vtype=gp.GRB.BINARY, name="x")
+        self.y = self.model.addVars(self.instance.relations, vtype=gp.GRB.BINARY, name="y")
+        self.u = self.model.addVars(self.instance.nodes, vtype=gp.GRB.CONTINUOUS, name="u")
 
         # Flow conservation constraints
-        model.addConstrs(
-            (gp.quicksum(x[i, j] for j in self.instance.delta_out[i]) == 1 for i in self.instance.nodes),
+        self.model.addConstrs(
+            (gp.quicksum(self.x[i, j] for j in self.instance.delta_out[i]) == 1 for i in self.instance.nodes),
             name="flow_conservation_out",
         )
-        model.addConstrs(
-            (gp.quicksum(x[j, i] for j in self.instance.delta_in[i]) == 1 for i in self.instance.nodes),
+        self.model.addConstrs(
+            (gp.quicksum(self.x[j, i] for j in self.instance.delta_in[i]) == 1 for i in self.instance.nodes),
             name="flow_conservation_in",
         )
 
         # Subtour elimination constraints
-        model.addConstrs(
+        self.model.addConstrs(
             (
-                u[i] - u[j] + self.instance.N * x[i, j] <= self.instance.N - 1
+                self.u[i] - self.u[j] + self.instance.N * self.x[i, j] <= self.instance.N - 1
                 for i, j in self.instance.edges
-                if i != 0 and j != 0
+                if j != 0
             ),
             name="subtour_elimination",
         )
 
         # At most one relation can be active in R_a
-        model.addConstrs(
-            (gp.quicksum(y[*b, *a] for b in self.instance.R_a[a]) <= x[a] for a in self.instance.R_a),
+        self.model.addConstrs(
+            (gp.quicksum(self.y[*b, *a] for b in self.instance.R_a[a]) <= self.x[a] for a in self.instance.R_a),
             name="max_one_relation",
         )
 
         # Relation r=(b,a) \in R_a is inactive if a or b are inactive
-        model.addConstrs(
-            (y[*b, *a] <= x[a] for a in self.instance.R_a for b in self.instance.R_a[a]),
+        self.model.addConstrs(
+            (self.y[*b, *a] <= self.x[a] for a in self.instance.R_a for b in self.instance.R_a[a]),
             name="relation_inactive_if_target_inactive",
         )
-        model.addConstrs(
-            (y[*b, *a] <= x[b] for a in self.instance.R_a for b in self.instance.R_a[a]),
+        self.model.addConstrs(
+            (self.y[*b, *a] <= self.x[b] for a in self.instance.R_a for b in self.instance.R_a[a]),
             name="relation_inactive_if_target_inactive",
         )
 
         # Relation r=(b,a) \in R_a is inactive if b after a in the tour
-        model.addConstrs(
+        self.model.addConstrs(
             (
-                u[b[0]] + 1 <= u[a[0]] + self.instance.N * (1 - y[*b, *a])
+                self.u[b[0]] + 1 <= self.u[a[0]] + self.instance.N * (1 - self.y[*b, *a])
                 for a in self.instance.R_a
                 for b in self.instance.R_a[a]
             ),
@@ -93,48 +94,74 @@ class GurobiModel:
                 for c in self.instance.R_a[a]:
                     if b == c:
                         continue
-                    # add two binary variables z_1 and z_2 to model the implication
-                    z_1[a, b, c] = model.addVar(
+                    # add two binary variables z_1 and z_2 to self.model the implication
+                    z_1[a, b, c] = self.model.addVar(
                         vtype=gp.GRB.BINARY,
                         name=f"z_1_{b}_{c}_{a}",
                     )
-                    z_2[a, b, c] = model.addVar(
+                    z_2[a, b, c] = self.model.addVar(
                         vtype=gp.GRB.BINARY,
                         name=f"z_2_{b}_{c}_{a}",
                     )
                     # If in the tour we have b -> c -> a, then y[b, a] = 0
-                    model.addConstr(u[b[0]] + 1 <= u[c[0]] + self.instance.N * (z_1[a, b, c]))
-                    model.addConstr(u[c[0]] <= u[b[0]] + self.instance.N * (1 - z_1[a, b, c]))
-                    model.addConstr(u[c[0]] + 1 <= u[a[0]] + self.instance.N * (z_2[a, b, c]))
-                    model.addConstr(u[a[0]] <= u[c[0]] + self.instance.N * (1 - z_2[a, b, c]))
-                    model.addConstr(y[*b, *a] <= z_1[a, b, c] + z_2[a, b, c])
+                    self.model.addConstr(self.u[b[0]] + 1 <= self.u[c[0]] + self.instance.N * (z_1[a, b, c]))
+                    self.model.addConstr(self.u[c[0]] <= self.u[b[0]] + self.instance.N * (1 - z_1[a, b, c]))
+                    self.model.addConstr(self.u[c[0]] + 1 <= self.u[a[0]] + self.instance.N * (z_2[a, b, c]))
+                    self.model.addConstr(self.u[a[0]] <= self.u[c[0]] + self.instance.N * (1 - z_2[a, b, c]))
+                    self.model.addConstr(self.y[*b, *a] <= z_1[a, b, c] + z_2[a, b, c])
 
         # Set the objective function
-        model.setObjective(
+        self.model.setObjective(
             gp.quicksum(
-                x[a] * self.instance.edges[a]
-                + gp.quicksum(y[*b, *a] * self.instance.relations[*b, *a] for b in self.instance.R_a[a])
+                self.x[a] * self.instance.edges[a]
+                + gp.quicksum(self.y[*b, *a] * self.instance.relations[*b, *a] for b in self.instance.R_a[a])
                 for a in self.instance.edges
             ),
             sense=gp.GRB.MINIMIZE,
         )
 
-        return model
+    def solve_model_with_parameters(self) -> None:
+        if self.model is None:
+            raise ValueError("Model is not formulated")
 
-    def solve_model_with_parameters(self, model: gp.Model) -> gp.Model:
         # set time limit to two minutes
-        model.setParam(gp.GRB.Param.TimeLimit, 120)
-        model.optimize()
+        self.model.setParam(gp.GRB.Param.TimeLimit, 120)
+        self.model.optimize()
 
-        status = model.status
+        status = self.model.status
         if status == gp.GRB.Status.INFEASIBLE:
             # run infeasibility analysis
-            model.computeIIS()
-            model.write("model.ilp")
+            self.model.computeIIS()
+            self.model.write("model.ilp")
             error_msg = "Model is infeasible"
             raise ValueError(error_msg)
 
-        return model
+    def get_original_objective(self) -> list[list, float]:
+        tour = sorted([i for i in self.instance.nodes if i != 0], key=lambda i: self.u[i].X)
+        tour = [0, *tour]
+
+        cost = 0
+        for edge in self.instance.edges:
+            if self.x[edge].X > 0.5:
+                cost += self.instance.edges[edge]
+
+        for relation in self.instance.relations:
+            if self.y[relation].X > 0.5:
+                cost += self.instance.relations[relation] - self.instance.offset
+
+        return tour, cost
+
+    def get_x(self) -> dict:
+        return self.x
+
+    def get_y(self) -> dict:
+        return self.y
+
+    def get_u(self) -> dict:
+        return self.u
+
+    def get_model(self) -> gp.Model:
+        return self.model
 
 
 def gurobi_main(instance_name: str) -> None:
@@ -145,22 +172,7 @@ def gurobi_main(instance_name: str) -> None:
     gurobi_model = GurobiModel(instance)
     model = gurobi_model.formulate()
     model = gurobi_model.solve_model_with_parameters(model)
-
-    u = model._u
-    x = model._x
-    y = model._y
-
-    tour = sorted([i for i in instance.nodes if i != 0], key=lambda i: u[i].X)
-    tour = [0, *tour]
-
-    cost = 0
-    for edge in instance.edges:
-        if x[edge].X > 0.5:
-            cost += instance.edges[edge]
-
-    for relation in instance.relations:
-        if y[relation].X > 0.5:
-            cost += instance.relations[relation] + instance.offset
+    tour, cost = gurobi_model.get_original_objective(model)
 
     instance.test_solution(tour, cost)
     instance.save_solution(tour, cost)
