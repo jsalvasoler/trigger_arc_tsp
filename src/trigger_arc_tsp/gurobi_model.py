04ea7332306f1dcc051c06f70f0b80cc4b1b2c5a
jsalvasoler <jsalvasoler@hotmail.com>
2024-08-11 18:41:28 +0200

    feat: first working formulation

--- src/trigger_arc_tsp/gurobi_model.py
+++ src/trigger_arc_tsp/gurobi_model.py
@@ -7,7 +7,7 @@ from trigger_arc_tsp.utils import INSTANCES_DIR
 
 
 def main() -> None:
-    instance = Instance.load_instance_from_file(os.path.join(INSTANCES_DIR, "examples", "example_1.txt"))
+    instance = Instance.load_instance_from_file(os.path.join(INSTANCES_DIR, "instances_release_1", "grf5.txt"))
     print(instance)
 
     model = gp.Model("TriggerArcTSP")
@@ -30,12 +30,94 @@ def main() -> None:
         name="subtour_elimination",
     )
 
-    # Set a random objective
-    model.setObjective(gp.quicksum(x) + gp.quicksum(y), sense=gp.GRB.MINIMIZE)
+    # At most one relation can be active in R_a
+    model.addConstrs(
+        (gp.quicksum(y[*b, *a] for b in instance.R_a[a]) <= x[a] for a in instance.R_a), name="max_one_relation"
+    )
+
+    # Relation r=(b,a) \in R_a is inactive if a or b are inactive
+    model.addConstrs(
+        (y[*b, *a] <= x[a] for a in instance.R_a for b in instance.R_a[a]), name="relation_inactive_if_target_inactive"
+    )
+    model.addConstrs(
+        (y[*b, *a] <= x[b] for a in instance.R_a for b in instance.R_a[a]), name="relation_inactive_if_target_inactive"
+    )
+
+    # Relation r=(b,a) \in R_a is inactive if b after a in the tour
+    model.addConstrs(
+        (u[b[0]] + 1 <= u[a[0]] + instance.N * (1 - y[*b, *a]) for a in instance.R_a for b in instance.R_a[a]),
+        name="trigger_before_arc",
+    )
+
+    # For different relations r=(b,a) and r'=(c,a) in R_a, if c comes after b
+    # in the tour, then y[b, a] <= y[c, a] (1 <= 1 or 0 <= 0 or 0 <= 1)
+    z_1, z_2 = {}, {}
+    for a in instance.R_a:
+        for b in instance.R_a[a]:
+            for c in instance.R_a[a]:
+                if b == c:
+                    continue
+                # add two binary variables z_1 and z_2 to model the implication
+                z_1[a, b, c] = model.addVar(
+                    vtype=gp.GRB.BINARY,
+                    name=f"z_1_{b}_{c}_{a}",
+                )
+                z_2[a, b, c] = model.addVar(
+                    vtype=gp.GRB.BINARY,
+                    name=f"z_2_{b}_{c}_{a}",
+                )
+                # If in the tour we have b -> c -> a, then y[b, a] = 0
+                model.addConstr(u[b[0]] + 1 <= u[c[0]] + instance.N * (z_1[a, b, c]))
+                model.addConstr(u[c[0]] <= u[b[0]] + instance.N * (1 - z_1[a, b, c]))
+                model.addConstr(u[c[0]] + 1 <= u[a[0]] + instance.N * (z_2[a, b, c]))
+                model.addConstr(u[a[0]] <= u[c[0]] + instance.N * (1 - z_2[a, b, c]))
+                model.addConstr(y[*b, *a] <= z_1[a, b, c] + z_2[a, b, c])
+
+    # Set the objective function
+    model.setObjective(
+        gp.quicksum(
+            x[a] * instance.edges[a] + gp.quicksum(y[*b, *a] * instance.relations[*b, *a] for b in instance.R_a[a])
+            for a in instance.edges
+        ),
+        sense=gp.GRB.MINIMIZE,
+    )
+
+    # # Force the solution with order
+    # model.addConstr(u[0] == 0)
+    # model.addConstr(u[2] == 1)
+    # model.addConstr(u[1] == 2)
+    # model.addConstr(u[4] == 3)
+    # model.addConstr(u[3] == 4)
 
     model.optimize()
 
-    print("Objective value:", model.objVal)
+    status = model.status
+    if status == gp.GRB.Status.INFEASIBLE:
+        # run infeasibility analysis
+        model.computeIIS()
+        model.write("model.ilp")
+        return
+
+    print("\n\n")
+    print(" - Solution:")
+    baseline_cost = 0
+    for i, j in instance.edges:
+        if x[i, j].x > 0.5:
+            print(f"Edge ({i}, {j}) is used")
+            baseline_cost += instance.edges[i, j]
+
+    print(f" - Baseline cost: {baseline_cost}")
+    for i, j, r, s in instance.relations:
+        if y[i, j, r, s].x > 0.5:
+            print(
+                f" - Relation ({i}, {j}) -> ({r}, {s}) triggers cost "
+                f"change + {instance.relations[i, j, r, s] - instance.offset}"
+            )
+            baseline_cost += instance.relations[i, j, r, s] - instance.offset
+        else:
+            print(f" - Relation ({i}, {j}) -> ({r}, {s}) is inactive")
+
+    print(f" - Total cost: {baseline_cost}")
 
 
 if __name__ == "__main__":
--- src/trigger_arc_tsp/instance.py
+++ src/trigger_arc_tsp/instance.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections import defaultdict
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -7,20 +8,34 @@ if TYPE_CHECKING:
 
 
 class Instance:
-    def __init__(self, N: int, edges: list, relations: dict) -> None:
+    def __init__(self, N: int, edges: list, relations: list) -> None:
         self.edges = edges
         self.relations = relations
 
         self.N = N
         self.nodes = set(range(self.N))
         self.A = len(edges)
-        self.R = len(relations)
+        self.R = len(self.relations)
+
+        self.R_a = defaultdict(list)
+        for b_0, b_1, a_0, a_1 in self.relations:
+            self.R_a[(a_0, a_1)].append((b_0, b_1))
 
         self.delta_in = {node: set() for node in self.nodes}
         self.delta_out = {node: set() for node in self.nodes}
         for i, j in edges:
             self.delta_in[j].add(i)
             self.delta_out[i].add(j)
+
+        self.rescale_costs_of_relations()
+
+    def rescale_costs_of_relations(self) -> None:
+        relative_costs = {rel: self.relations[rel] - self.edges[(rel[2], rel[3])] for rel in self.relations}
+        max_rel_cost = max(relative_costs.values())
+        if max_rel_cost > 0:
+            for rel in self.relations:
+                self.relations[rel] = relative_costs[rel] - max_rel_cost - 1
+        self.offset = -max_rel_cost - 1
 
     @staticmethod
     def load_instance_from_file(file_path: os.PathLike) -> Instance:
@@ -37,9 +52,9 @@ class Instance:
             relations = {}
             for line in lines[1 + A : 1 + A + R]:
                 _, _, from_trigger, to_trigger, _, from_arc, to_arc, cost = map(float, line.split())
-                relations[(int(from_arc), int(to_arc))] = (int(from_trigger), int(to_trigger), cost)
+                relations[(int(from_trigger), int(to_trigger), int(from_arc), int(to_arc))] = cost
 
-            assert int(line[0]) == R - 1
+            assert int(line.split(" ")[0]) == R - 1
 
         return Instance(N, edges, relations)
 
