3846cd23636a8d83dfd1f24694156522e1854725
jsalvasoler <jsalvasoler@hotmail.com>
2024-08-18 18:06:29 +0200

    refactor: rename he to heuristic_effort

--- src/trigger_arc_tsp/__main__.py
+++ src/trigger_arc_tsp/__main__.py
@@ -13,7 +13,7 @@ def cli() -> None:
 @cli.command(name="gurobi", help="Evaluate the model", context_settings={"show_default": True})
 @click.argument("instance", type=str, required=True)
 @click.option("--time_limit_sec", type=int, default=60)
-@click.option("--he", type=float, default=0.05)
+@click.option("--heuristic_effort", type=float, default=0.05)
 @click.option("--mip_start", type=bool, is_flag=True, default=False, help="Provide a MIP start")
 @click.option(
     "--relax_obj_modeling", type=bool, is_flag=True, default=False, help="Relax integrality of objective modeling"
@@ -22,7 +22,7 @@ def cli() -> None:
 def evaluate_command(
     instance: str | None,
     time_limit_sec: int = 60,
-    he: float = 0.05,
+    heuristic_effort: float = 0.05,
     *,
     mip_start: bool = False,
     relax_obj_modeling: bool = False,
@@ -32,7 +32,7 @@ def evaluate_command(
     gurobi_main(
         instance,
         time_limit_sec=time_limit_sec,
-        heuristic_effort=he,
+        heuristic_effort=heuristic_effort,
         mip_start=mip_start,
         relax_obj_modeling=relax_obj_modeling,
         read_model=read_model,
